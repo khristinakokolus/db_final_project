6b5ef5e22d7daea4ff9fb9b9b867921eef3fd19b
ReyBroncas <andriksmen@gmail.com>
2021-04-25 18:24:31 +0300

    added data entries to fainted, released, bedstatus, escaped tables.

--- sql/database_creation.sql
+++ sql/database_creation.sql
@@ -12,6 +12,7 @@
 -- drop table alcoholic;
 -- drop table incident;
 
+
 CREATE TABLE alcoholic (
   alcoholic_id SERIAL PRIMARY KEY,
   first_name VARCHAR(20) NOT NULL,
@@ -31,7 +32,7 @@ INSERT INTO alcoholic(first_name, last_name, created_at, updated_at) VALUES
 ('David', 'Johnson', '2021-04-12 13:30:12', CURRENT_TIMESTAMP),
 ('Luke', 'Williams', '2021-04-08 10:05:47', CURRENT_TIMESTAMP),
 ('Leo', 'Jones', '2021-02-11 08:41:29', CURRENT_TIMESTAMP);
-
+select * from alcoholic;
 CREATE TABLE officer (
   officer_id SERIAL PRIMARY KEY,
   first_name VARCHAR(20) NOT NULL,
@@ -117,52 +118,6 @@ CREATE TABLE incident_info (
   FOREIGN KEY(incident_id) REFERENCES incident(incident_id)
 );
 
-CREATE TABLE fainted (
-  fainted_id SERIAL PRIMARY KEY,
-  alcoholic_id INT,
-  happened_at DATE NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  FOREIGN KEY(alcoholic_id) REFERENCES alcoholic(alcoholic_id)
-);
-
-CREATE TABLE trailed (
-  trailed_id SERIAL PRIMARY KEY,
-  incident_id INT,
-  alcoholic_id INT,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  FOREIGN KEY(incident_id) REFERENCES incident(incident_id),
-  FOREIGN KEY(alcoholic_id) REFERENCES alcoholic(alcoholic_id)
-);
-
-CREATE TABLE bed_status (
-  exhange_id SERIAL PRIMARY KEY,
-  bed_id INT,
-  alcoholic_id INT,
-  officer_id INT,
-  stay_start DATE NOT NULL,
-  stay_end DATE NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  FOREIGN KEY(bed_id) REFERENCES bed(bed_id),
-  FOREIGN KEY(alcoholic_id) REFERENCES alcoholic(alcoholic_id),
-  FOREIGN KEY(officer_id) REFERENCES officer(officer_id)
-);
-
-CREATE TABLE released (
-  released_id SERIAL PRIMARY KEY,
-  old_bed_id INT,
-  alcoholic_id INT,
-  officer_id INT,
-  happened_at DATE NOT NULL,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
-  FOREIGN KEY(old_bed_id) REFERENCES bed(bed_id),
-  FOREIGN KEY(alcoholic_id) REFERENCES alcoholic(alcoholic_id),
-  FOREIGN KEY(officer_id) REFERENCES officer(officer_id)
-);
-
 CREATE TABLE capture (
   capture_id SERIAL PRIMARY KEY,
   officer_id INT,
@@ -176,13 +131,97 @@ CREATE TABLE capture (
   FOREIGN KEY(bed_id) REFERENCES bed(bed_id)
 );
 
-CREATE TABLE escaped (
-  escaped_id SERIAL PRIMARY KEY,
-  old_bed_id INT,
+CREATE TABLE trailed (
+  trailed_id SERIAL PRIMARY KEY,
+  incident_id INT,
   alcoholic_id INT,
-  happened_at DATE NOT NULL,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
-  FOREIGN KEY(old_bed_id) REFERENCES bed(bed_id),
+  FOREIGN KEY(incident_id) REFERENCES incident(incident_id),
+  FOREIGN KEY(alcoholic_id) REFERENCES alcoholic(alcoholic_id)
+);
+
+CREATE TABLE fainted (
+  fainted_id SERIAL PRIMARY KEY,
+  alcoholic_id INT,
+  happened_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  FOREIGN KEY(alcoholic_id) REFERENCES alcoholic(alcoholic_id)
+);
+
+INSERT INTO fainted(alcoholic_id, happened_at, created_at, updated_at) VALUES
+(5,'2021-01-10 03:41:14', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1,'2021-01-16 04:20:00', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1,'2021-01-17 01:15:27', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1,'2021-01-17 04:20:00', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(10,'2021-02-12 03:41:29', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(3,'2021-03-15 08:28:01', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(3,'2021-03-16 11:13:05', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4,'2021-03-17 04:20:00', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(9,'2021-04-09 07:02:12', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(8,'2021-04-13 04:30:12', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+CREATE TABLE bed_status (
+  bed_staus_id SERIAL PRIMARY KEY,
+  bed_id INT,
+  alcoholic_id INT,
+  officer_id INT,
+  stay_start TIMESTAMP NOT NULL,
+  stay_end TIMESTAMP,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  FOREIGN KEY(bed_id) REFERENCES bed(bed_id),
+  FOREIGN KEY(alcoholic_id) REFERENCES alcoholic(alcoholic_id),
+  FOREIGN KEY(officer_id) REFERENCES officer(officer_id)
+);
+
+INSERT INTO bed_status(bed_id, alcoholic_id, officer_id, stay_start, stay_end, created_at, updated_at) VALUES
+(1, 1, 5, '2021-01-15 08:01:00', '2021-02-01 08:00:00', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(2, 2, 1, '2021-04-24 17:25:44', '2021-08-11 00:12:58', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(3, 3, 6, '2021-03-14 14:28:25', '2021-08-25 18:01:25', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(4, 4, 7, '2021-03-16 02:27:03', '2021-03-20 02:27:03', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(1, 4, 7, '2021-03-20 02:27:03', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(5, 5, 4, '2021-01-09 15:41:35', '2021-06-28 04:26:57', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(6, 6, 3, '2021-04-25 21:44:58', '2021-06-09 13:45:15', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(7, 7, 6, '2021-03-01 09:58:18', '2021-06-07 02:12:19', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(8, 8, 10, '2021-04-12 13:30:12', '2021-05-01 14:18:49', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(9, 9, 2, '2021-04-08 10:05:47', '2021-07-24 06:31:19', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(10 ,10, 9, '2021-02-11 08:41:29', '2021-06-27 01:01:03', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+CREATE TABLE released (
+  released_id SERIAL PRIMARY KEY,
+  bed_id INT,
+  alcoholic_id INT,
+  officer_id INT,
+  happened_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  FOREIGN KEY(bed_id) REFERENCES bed(bed_id),
+  FOREIGN KEY(alcoholic_id) REFERENCES alcoholic(alcoholic_id),
+  FOREIGN KEY(officer_id) REFERENCES officer(officer_id)
+);
+
+INSERT INTO released(bed_id, alcoholic_id, officer_id, happened_at, created_at, updated_at) VALUES
+(1, 1, 5, '2021-02-01 08:00:00', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(2, 2, 1, '2021-08-11 00:12:58', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(3, 3, 6, '2021-08-25 18:01:25', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(5, 5, 4, '2021-06-28 04:26:57', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(6, 6, 3, '2021-06-09 13:45:15', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(7, 7, 6, '2021-06-07 02:12:19', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(8, 8, 10, '2021-05-01 14:18:49', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+(9, 9, 2, '2021-07-24 06:31:19', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+CREATE TABLE escaped (
+  escaped_id SERIAL PRIMARY KEY,
+  bed_id INT,
+  alcoholic_id INT,
+  happened_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  FOREIGN KEY(bed_id) REFERENCES bed(bed_id),
   FOREIGN KEY(alcoholic_id) REFERENCES alcoholic(alcoholic_id)	
 );
+
+INSERT INTO escaped(bed_id, alcoholic_id, happened_at, created_at, updated_at) VALUES
+(10, 10, '2021-06-27 01:01:03', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
